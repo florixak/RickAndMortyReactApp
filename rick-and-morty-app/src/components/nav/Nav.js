a351e750f40cc1a5d8b013a7466b6f149408fb89
florixak <florixakcz@seznam.cz>
2023-05-24 13:49:46 +0200

    React-Router-Dom update

--- rick-and-morty-app/src/components/nav/Nav.js
+++ rick-and-morty-app/src/components/nav/Nav.js
@@ -1,5 +1,7 @@
 import "./Nav.css"
 
+import { Link } from "react-router-dom"
+
 import npm_api_logo from "../../images/ram-api-logo.png";
 import florix_logo from "../../images/florix-logo.jpg";
 
@@ -8,8 +10,14 @@ const Nav = () => {
 
     return (
         <div className="nav">
-            <img src={npm_api_logo} alt="" title="Rick and Morty API" onClick={() => {window.open("https://rickandmortyapi.com/")}} />
-            <img src={florix_logo} alt="" title="Creator" onClick={() => {window.open("https://www.florixak.tk/")}} />
+            <div className="nav-left">
+                <p><Link to="/">Home</Link></p>
+            </div>
+            <div className="nav-right">
+                <img src={npm_api_logo} alt="" title="Rick and Morty API" onClick={() => {window.open("https://rickandmortyapi.com/")}} />
+                <img src={florix_logo} alt="" title="Creator" onClick={() => {window.open("https://www.florixak.tk/")}} />
+            </div>
+            
         </div>
     )
 }
---
+++ rick-and-morty-app/src/data/data.js
@@ -0,0 +1,7 @@
+
+const data = () => {
+    author = "FloriXak",
+    author_website = "https://www.florixak.tk/"
+}
+
+export default data
