a7a9881a8b6a1e1b2e709bda408bd79470e731ff
florixak <florixakcz@seznam.cz>
2024-06-18 12:31:43 +0200

    Update CardList.jsx

--- rick-and-morty-react-vite-app/src/components/CardList.jsx
+++ rick-and-morty-react-vite-app/src/components/CardList.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import PagingButton from "./PagingButton";
 
 const itemsPerPage = 12;
@@ -7,6 +8,9 @@ const itemsPerPage = 12;
 export default function CardList({ children, title, handleData, url }) {
   const [page, setPage] = useState(1);
   const [info, setInfo] = useState({});
+  const [inputValue, setInputValue] = useState(undefined);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get(`${url}?page=${page}`).then((response) => {
@@ -16,6 +20,17 @@ export default function CardList({ children, title, handleData, url }) {
       console.log(response.data.info);
     });
   }, [page]);
+
+  const handleInputValue = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue) {
+      navigate(`/characters/${inputValue}`);
+    }
+  };
 
   const setNextPage = () => {
     setPage((prevPage) => {
@@ -34,6 +49,19 @@ export default function CardList({ children, title, handleData, url }) {
   return (
     <div className="w-[90%] h-full flex justify-center items-center gap-5 flex-col">
       <h1>{title}</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          className="text-center rounded-lg p-2 text-black"
+          type="text"
+          placeholder="Specify id"
+          value={inputValue}
+          onChange={handleInputValue}
+        />
+        <button className="hidden" type="submit">
+          Search
+        </button>
+      </form>
+
       <div className="grid grid-cols-2 gap-10 sm:grid-cols-4">
         {children}
       </div>
