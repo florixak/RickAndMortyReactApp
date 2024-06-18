fc597c50df739d78fedbb25e2618839ff290811e
florixak <florixakcz@seznam.cz>
2024-06-18 22:10:26 +0200

    Update

--- rick-and-morty-react-vite-app/src/components/Characters/CharacterCard.jsx
+++ rick-and-morty-react-vite-app/src/components/Characters/CharacterCard.jsx
@@ -1,14 +1,8 @@
 import Card from "../Card";
+import CharacterStatus from "./CharacterStatus";
 
 export default function CharacterCard({ data }) {
-
   const { image, name, id, status, species, gender, origin, location } = data;
-
-  const statusStyle = () => {
-    if (data.status === "Alive") return "text-green-400";
-    else if (data.status === "Dead") return "text-red-400";
-    else return "text-orange-400";
-  };
 
   return (
     <Card>
@@ -16,7 +10,7 @@ export default function CharacterCard({ data }) {
       <h1 className="text-xl">{name}</h1>
       <div className="flex flex-col items-start justify-start">
         <p>ID: {id}</p>
-        <p className={statusStyle()}>Status: {status}</p>
+        <p>Status: <CharacterStatus status={status} /></p>
         <p>Species: {species}</p>
         <p>Gender: {gender}</p>
         {origin && <p>Origin: {origin.name}</p>}
---
+++ rick-and-morty-react-vite-app/src/components/Characters/CharacterStatus.jsx
@@ -0,0 +1,9 @@
+export default function CharacterStatus({ status }) {
+  const statusStyle = () => {
+    if (status === "Alive") return "text-green-400";
+    else if (status === "Dead") return "text-red-400";
+    else return "text-orange-400";
+  };
+
+  return <span className={`${statusStyle()} text-xl items-center justify-center`}>•</span>;
+}
