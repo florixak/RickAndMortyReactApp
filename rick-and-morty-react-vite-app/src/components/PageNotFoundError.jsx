8bf0689eeda93399f4fbbf0b4f1e399018bef5f3
florixak <florixakcz@seznam.cz>
2024-06-22 08:14:11 +0200

    Update

--- rick-and-morty-react-vite-app/src/components/PageNotFoundError.jsx
+++ rick-and-morty-react-vite-app/src/components/PageNotFoundError.jsx
@@ -3,7 +3,7 @@ import PageNotFoundErrorLogo from "../assets/PageNotFoundLogo.png"
 export default function PageNotFoundError() {
   return (
     <div className="w-full flex flex-col items-center justify-center gap-8">
-      <h1 className="text-red-500">Page not found error</h1>
+      <h1 className="font-bold">Page not found error</h1>
       <img src={PageNotFoundErrorLogo} width={320} alt="Page not found error logo" />
     </div>
   );
