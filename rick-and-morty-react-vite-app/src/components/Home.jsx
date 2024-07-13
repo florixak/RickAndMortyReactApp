b9d0a05dba2f21b95684523deae3e3e01cdd553b
florixak <florixakcz@seznam.cz>
2024-07-13 09:25:59 +0200

    Update

--- rick-and-morty-react-vite-app/src/components/Home.jsx
+++ rick-and-morty-react-vite-app/src/components/Home.jsx
@@ -57,7 +57,7 @@ const Paragraph = ({ children }) => {
 export default function Home() {
   return (
     <main className="w-full mt-[50px] md:h-[100%] flex flex-col justify-center items-center gap-16 text-primary">
-      <section className="max-w-[1000px] flex flex-col gap-36 mb-16 items-center justify-center">
+      <section className="max-w-[1050px] flex flex-col gap-36 mb-16 items-center justify-center">
         <section className="flex flex-col lg:flex-row gap-5 h-auto">
           {/* About story */}
           <AnimatedSectionInView
@@ -91,9 +91,8 @@ export default function Home() {
             className="lg:w-[40vh] flex flex-col gap-3 items-center justify-center p-5 bg-light-slate rounded-3xl shadow-black shadow-lg"
             delay={0.3}
           >
-            <div className="bg-dark-background w-auto p-1 rounded-3xl">
+            <div className="w-auto bg-dark-background p-1 rounded-3xl">
               <img
-                width={240}
                 className="rounded-3xl shadow-black shadow-lg m-auto"
                 src={RickAndMortyTVImage}
                 alt=""
