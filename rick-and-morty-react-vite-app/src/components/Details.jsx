e39e37c3f0e50b21285f69c4d77bc8bf75dba8f1
florixak <florixakcz@seznam.cz>
2024-07-18 06:57:08 +0200

    Update Details.jsx

--- rick-and-morty-react-vite-app/src/components/Details.jsx
+++ rick-and-morty-react-vite-app/src/components/Details.jsx
@@ -1,6 +1,6 @@
 export default function Details({ children }) {
   return (
-    <div className="w-full sm:max-w-[50%] lg:max-w-[30%] flex flex-col items-center m-auto p-5 gap-5 bg-light-slate text-primary rounded-3xl shadow-black shadow-md">
+    <div className="w-full sm:max-w-[75%] lg:max-w-[35%] flex flex-col xl:flex-row items-center justify-evenly m-auto p-5 gap-5 bg-light-slate text-primary rounded-3xl shadow-black shadow-md">
       {children}
     </div>
   );
