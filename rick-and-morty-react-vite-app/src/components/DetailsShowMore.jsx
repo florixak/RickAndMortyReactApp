987deb783bae7e90a2c5eef066259868e2183797
florixak <florixakcz@seznam.cz>
2024-07-18 13:34:27 +0200

    Add show more animation

--- rick-and-morty-react-vite-app/src/components/DetailsShowMore.jsx
+++ rick-and-morty-react-vite-app/src/components/DetailsShowMore.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 
 export default function DetailsShowMore({ list, listName, getURL }) {
@@ -10,22 +11,28 @@ export default function DetailsShowMore({ list, listName, getURL }) {
   const displayedList = showMore ? list : list && list.slice(0, 10);
 
   return (
-    <div className="flex flex-wrap gap-2 max-w-[350px]">
+    <motion className="flex flex-wrap gap-2 max-w-[350px]">
       <span className="font-semibold">{listName}:</span>
-      {displayedList &&
-        displayedList.map((item, index) => {
-          const num = item.match(/\/(\d+)$/)[1];
-          return (
-            <span
-              onClick={() => navigate(getURL(num))}
-              key={item}
-              className="hover:cursor-pointer hover:underline"
-            >
-              {num}
-              {index < displayedList.length - 1 ? "," : ""}
-            </span>
-          );
-        })}
+      <AnimatePresence>
+        {displayedList &&
+          displayedList.map((item, index) => {
+            const num = item.match(/\/(\d+)$/)[1];
+            return (
+              <motion.span
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                onClick={() => navigate(getURL(num))}
+                key={item}
+                className="hover:cursor-pointer hover:underline"
+              >
+                {num}
+                {index < displayedList.length - 1 ? "," : ""}
+              </motion.span>
+            );
+          })}
+      </AnimatePresence>
+
       {list && list.length > 10 && (
         <button
           onClick={toggleShowMore}
@@ -34,6 +41,6 @@ export default function DetailsShowMore({ list, listName, getURL }) {
           {showMore ? "Show Less" : "Show More"}
         </button>
       )}
-    </div>
+    </motion>
   );
 }
